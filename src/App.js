766fb9ea98298bc02330edb51e318b6cd667b23b
Abdullo <abdullofayzullayev68@gmail.com>
2022-08-06 18:35:01 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [city, setCity] = useState(" ");
+  const [weather, setWeather] = useState({});
+
+  useEffect(() => {
+    if (city !== " ") {
+      axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=25ccd97dd292e0ee187017a56a07c519&units=metric`
+        )
+        .then(function (response) {
+          setWeather(response.data);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+  }, [city]);
+
+  console.log(weather);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        onKeyUp={(evt) => {
+          if (evt.code === "Enter") {
+            setCity(evt.target.value);
+          }
+        }}
+      />
+
+      <h1 className="cityName">{weather.name}</h1>
+      <p className="temp">
+        Temperature: <span className="temp__data">{weather?.main?.temp}</span>
+      </p>
     </div>
   );
 }
